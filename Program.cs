28d6934563e5c2c0c186960b7b7d1d353e763305
KrisiR10 <pishtovka123@gmail.com>
2025-09-22 13:35:37 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace zadacha_5
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //zadacha 5
+            Console.Write("Vuvedete godishnata taksa za trenirovka po ski: ");
+            double fee = double.Parse(Console.ReadLine());
+
+            double ski = fee * 0.60;
+            double equipment = ski * 0.80;
+            double helmet = equipment / 4.0;
+            double accessories = helmet / 5.0;
+
+            double total = fee + ski + equipment + helmet + accessories;
+
+            Console.WriteLine($"total = { total:F2}");
+        }
+    }
+}
