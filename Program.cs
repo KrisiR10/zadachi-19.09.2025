8ed9c5f844c93e5ae16cfa70fe6ce50d6ca569d1
KrisiR10 <pishtovka123@gmail.com>
2025-09-22 13:39:51 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Vuvedete marka kola: ");
+            string model = Console.ReadLine();
+
+            Console.Write("Vuvedete izminati kilometri: ");
+            double km = double.Parse(Console.ReadLine());
+
+            Console.Write("Vuvedete razhod gorivo: ");
+            double fuel = double.Parse(Console.ReadLine());
+
+            double srednaSkorost = (fuel / km) * 100;
+            Console.WriteLine("Vashiqt sreden razhod za 100km e" + " " + srednaSkorost + "l/km, vse pak e " + model + "!");
+        }
+    }
+}
